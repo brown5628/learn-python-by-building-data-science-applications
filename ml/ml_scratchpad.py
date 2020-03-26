139e841944e7e3eecb0c1f4bcde4cd84e4fe3d2c
brown5628 <brown5628@gmail.com>
2020-03-26 06:35:06 -0400

    complete ml ch

--- ml/ml_scratchpad.py
+++ ml/ml_scratchpad.py
@@ -1,9 +1,17 @@
 # %%
 import pandas as pd
-import numpy as np
+# import numpy as np
 import altair as alt
+# import pydotplus
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
+from sklearn.preprocessing import scale, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, median_absolute_error
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+# from io import StringIO
+# from IPython.display import Image
 
 # %%
 data = pd.read_csv("../data/EF_battles_corrected.csv", parse_dates=["start", "end"])
@@ -95,5 +103,127 @@ centroids.index += 1
 
 # %%
 centroids
+
+# %%
+data = pd.read_csv("../data/EF_battles_corrected.csv", parse_dates=["start", "end"])
+
+data["end_num"] = (data["end"].dt.year - 1938) * 12 + data["end"].dt.month
+data["start_num"] = (data["start"].dt.year - 1938) * 12 + data["start"].dt.month
+data["duration"] = (data["end_num"] - data["start_num"]).clip(lower=1)
+
+cols = [
+    "allies_infantry",
+    "axis_infantry",
+    "allies_tanks",
+    "axis_tanks",
+    "allies_guns",
+    "axis_guns",
+]
+
+data["result_num"] = data["result"].map({"axis": -1, "allies": 1}).fillna(0)
+
+data["result_num"].value_counts()
+
+# %%
+mask = data[cols].isnull().any(1)
+X = data.loc[~mask, cols]
+y = data.loc[~mask, "result_num"]
+
+# %%
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state=2018)
+
+# %%
+s = StandardScaler()
+
+Xtrain = s.fit_transform(Xtrain)
+Xtests = s.transform(Xtest)
+
+# %%
+model = KNeighborsClassifier(n_neighbors=5)
+model.fit(Xtrain, ytrain)
+
+
+# %%
+ypred1 = model.predict(Xtest)
+accuracy_score(ytest, ypred1)
+
+# %%
+Xtest.head(1)
+
+# %%
+Xtrain.iloc[model.kneighbors(Xtest.head(1))[1][0]]
+
+# %%
+cols = [
+    "allies_infantry",
+    "axis_infantry",
+    "allies_tanks",
+    "axis_tanks",
+    "allies_guns",
+    "axis_guns",
+    "start_num",
+]
+
+# %%
+mask = data[cols + ["allies killed"]].isnull().any(1)
+
+# %%
+y = data.loc[~mask, "allies killed"]
+X = data.loc[~mask, cols]
+
+# %%
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.3, random_state=2019)
+
+# %%
+model = LinearRegression()
+model.fit(Xtrain, ytrain)
+
+# %%
+ypred = model.predict(Xtest)
+median_absolute_error(ytest, ypred)
+
+# %%
+(ypred - ytest)
+
+# %%
+pd.Series(model.coef_, index=X.columns)
+
+# %%
+cols = [
+    "allies_infantry",
+    "axis_infantry",
+    "allies_tanks",
+    "axis_tanks",
+    "allies_guns",
+    "axis_guns",
+]
+
+# %%
+mask = data[cols].isnull().any(1)
+X = data.loc[~mask, cols]
+y = data.loc[~mask, "result_num"]
+
+# %%
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state=2019)
+
+# %%
+tree_model = DecisionTreeClassifier(random_state=2019)
+tree_model.fit(Xtrain, ytrain)
+
+# %%
+accuracy_score(ytest, tree_model.predict(Xtest))
+
+# %%
+tree_model.predict(Xtest)
+
+# %%
+# dot_data = StringIO()
+
+# export_graphviz(tree_model, out_file=dot_data,
+#                 filled=True, rounded=True,
+#                 special_characters=True, feature_names=cols)
+
+# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+# Image(graph.create_png())
 
 # %%
